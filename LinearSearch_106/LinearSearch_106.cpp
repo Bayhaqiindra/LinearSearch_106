59d83c7bc514652330d9ed88be4396dd7940408f
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-13 15:09:03 +0700

    langkah algoritma 2,8.3 dan 4

--- LinearSearch_106/LinearSearch_106.cpp
+++ LinearSearch_106/LinearSearch_106.cpp
@@ -47,25 +47,3 @@ void LinerSearch()
 		for (i = 0; i < n; i++)			// Langkah no 2 8 3 & 4
 		{
 			ctr++;
-			if (arr[i] == item)			// Langkah no 5 Algoritma
-			{
-				cout << "/n" << item << " found at postion " << (i + 1) << endl;
-				break;
-			}
-		}
-
-		if (i == n)								//Langkah no 5 Algoritma
-			cout << "\n" << item << " not found in the array\n";
-		cout << "\nNumber of comparisons: " << ctr << endl;
-
-		cout << "\nContinue Search (y/n): ";
-		cin >> ch;
-	} while ((ch == 'y') || (ch == 'y'));				
-}
-
-
-int main()
-{
-	input();
-	LinerSearch();
-}
